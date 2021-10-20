26eb4723daca48727bfdf1891905139905e14fe7
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-20 10:59:29 -0300

    feat: creates last part of user authentication

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import prismaClient from '../prisma';
-
-
+import { sign } from 'jsonwebtoken'
+ 
 interface IAccessTokenResponse {
   access_token: string;
 }
@@ -52,7 +52,23 @@ class AuthenticateUserService {
         }
       })
     }
-    return response.data; 
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id
+       }
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: '1d'
+      }
+    
+    )
+    return { token, user } 
 
   }
 }
