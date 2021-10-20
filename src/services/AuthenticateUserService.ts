4eeaa82955ffcd3e5eea096f03cb1b90ec67e1d9
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-20 10:43:31 -0300

    feat: verify user existence and generate token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import prismaClient from '../prisma';
 
 
 interface IAccessTokenResponse {
@@ -33,7 +33,25 @@ class AuthenticateUserService {
         authorization: `Bearer ${accessTokenResponse.access_token}`
       }
     })
+ 
+    const { login, id, avatar_url, name } = response.data
+    
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    })
 
+    if(!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name
+        }
+      })
+    }
     return response.data; 
 
   }
