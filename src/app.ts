42f00fd3cc7a3073d847e83e0a9ff478bdf05c6b
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-20 10:21:06 -0300

    feat: creates interfaces to get user data

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import { router } from './routes'
 
 const app = express();
 
+app.use(express.json())
+
 app.use(router);
 
 app.get("/github", (request, response) => {
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,23 @@
 import axios from 'axios';
 
+
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string,
+  login: string,
+  id: number,
+  name: string
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -12,10 +25,16 @@ class AuthenticateUserService {
       },
       headers: {
         "Accept": "application/json"
+      },
+    });
+    
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`
       }
     })
 
-    return response.data;  //quando utilizamos o axios, toda informação retornada está contida dentro do data
+    return response.data; 
 
   }
 }
