7b4c19bd41e6b65d63007c15a08e45358a6b55aa
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-20 19:15:32 -0300

    feat: configurates websocket

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import 'dotenv/config'
 import express from 'express';
-import { Server } from "socket.io"
+import http from 'http';
+import cors from 'cors';
+
+import { Server } from "socket.io";
 
 import { router } from './routes'
 
+
 const app = express();
+
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", socket => {
+  console.log(`Usuário conectado no socket ${socket.id }`)
+})
 
 app.use(express.json())
 
@@ -20,4 +38,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code);
 })
 
-app.listen(4000, () => console.log(`🚀 Server is running on PORT 4000`))
+export { serverHttp, io };
