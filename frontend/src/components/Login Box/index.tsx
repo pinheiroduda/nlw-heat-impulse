7449c76061e2c6413d52b5393a075930b111c8c9
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-21 20:30:59 -0300

    feat: adds redirect to github for authorization when button is clicked

--- frontend/src/components/Login Box/index.tsx
+++ frontend/src/components/Login Box/index.tsx
@@ -3,10 +3,12 @@ import { VscGithubInverted } from 'react-icons/vsc'
 import styles from './styles.module.scss'
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=944e58d9fac9085b8458`
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com o GitHub
       </a>
