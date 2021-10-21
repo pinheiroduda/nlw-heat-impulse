edc7cbaf143c59de36554a6a3cfa9cc20a1f6062
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-21 20:41:43 -0300

    feat: removes github code when it exists

--- frontend/src/components/Login Box/index.tsx
+++ frontend/src/components/Login Box/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react'
 import { VscGithubInverted } from 'react-icons/vsc'
 
 import styles from './styles.module.scss'
 
 export function LoginBox() {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=944e58d9fac9085b8458`
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+      
+      window.history.pushState({}, '', urlWithoutCode);
+    }
+  }, [])
 
   return (
     <div className={styles.loginBoxWrapper}>
