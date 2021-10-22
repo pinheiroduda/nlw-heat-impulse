05866e61b5d8c18b478341e50e6bf20bde30f72e
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-22 00:05:59 -0300

    feat: saves user message when form button is clicked

--- frontend/src/components/SendMessageForm/index.tsx
+++ frontend/src/components/SendMessageForm/index.tsx
@@ -1,10 +1,22 @@
-import { useContext } from 'react'
+import { FormEvent, useContext, useState } from 'react'
 import { VscGithubInverted, VscSignOut } from 'react-icons/vsc'
 import { AuthContext } from '../../contexts/auth'
+import { api } from '../../services/api'
 import styles from './styles.module.scss'
 
 export function SendMessageForm() {
   const { user, signOut } = useContext(AuthContext)
+  const [message, setMessage] = useState('')
+
+  async function handleSendMessage(event: FormEvent) {
+    event.preventDefault();
+
+    if (!message.trim()) {
+      return;
+    }
+
+    await api.post('messages', { message })
+  }
 
   return (
     <div className={styles.sendMessageFormWrapper}>
@@ -22,10 +34,15 @@ export function SendMessageForm() {
         </strong>
       </header>
 
-      <form className={styles.sendMessageForm}>
+      <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
         <label htmlFor="message">Mensagem</label>
-        <textarea name="message" id="message" placeholder="Qual a sua expectativa para o evento?"></textarea>
-
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Qual a sua expectativa para o evento?"
+          onChange={event => setMessage(event.target.value)}
+          value={message}
+        />
         <button type="submit">Enviar mensagem</button>
       </form>
     </div>
