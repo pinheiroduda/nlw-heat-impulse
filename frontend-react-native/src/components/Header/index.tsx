b6d0da632b5601d087e9452ee12d3a18a6c41091
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-23 15:22:46 -0300

    feat: creates header component

---
+++ frontend-react-native/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+  View,
+  Text
+} from 'react-native';
+
+import { styles } from './styles';
+
+import LogoSvg from '../../assets/logo.svg';
+
+
+export function Header(){
+  return (
+    <View style={styles.container}>
+      <LogoSvg />
+
+      <Text>Sair</Text>
+
+    </View>
+  );
+}
--- frontend-react-native/src/screens/Home/index.tsx
+++ frontend-react-native/src/screens/Home/index.tsx
@@ -1,12 +1,14 @@
 import  React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import { Header } from '../../components/Header';
 
 import { styles } from './style';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Olá!</Text>
+      <Header />
     </View>
   )
 }
