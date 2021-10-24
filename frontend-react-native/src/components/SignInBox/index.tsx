35196673d87d470a6862a78748fe94aa3926bc17
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-24 00:41:27 -0300

    feat: creates sign in component

---
+++ frontend-react-native/src/components/SignInBox/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  View
+} from 'react-native';
+
+import { Button } from '../Button'
+import { COLORS } from '../../theme';
+
+import { styles } from './styles';
+
+export function SignInBox(){
+  return (
+    <View style={styles.container}>
+
+      <Button
+        title="ENTRAR COM O GITHUB"
+        color={COLORS.BLACK_PRIMARY}
+        backgroundColor={COLORS.YELLOW}
+      />
+
+    </View>
+  );
+}
