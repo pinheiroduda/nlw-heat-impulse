fbc937ef3ef30fd3b4e8888c9025b0cdeae89ce0
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-24 10:19:45 -0300

    feat: add github user image

--- frontend-react-native/src/components/Header/index.tsx
+++ frontend-react-native/src/components/Header/index.tsx
@@ -6,9 +6,12 @@ import { UserPhoto } from '../UserPhoto';
 import { styles } from './styles';
 
 import LogoSvg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 
 export function Header(){
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
       <LogoSvg />
@@ -18,7 +21,7 @@ export function Header(){
           <Text style={styles.logoutText}>Sair</Text>
         </TouchableOpacity>
 
-        <UserPhoto imageUri='https://github.com/pinheiroduda.png'/>
+        <UserPhoto imageUri={user?.avatar_url}/>
       </View>
     </View>
   );
