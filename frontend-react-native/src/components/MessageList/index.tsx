a4975f126c9ce24b9df7d1c407490760bbff24ca
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-24 11:49:13 -0300

    fix: real time user messages

--- frontend-react-native/src/components/MessageList/index.tsx
+++ frontend-react-native/src/components/MessageList/index.tsx
@@ -3,11 +3,13 @@ import { ScrollView } from 'react-native';
 import { api } from '../../services/api';
 import io from 'socket.io-client';
 
+import { MESSAGES_EXAMPLE } from '../../utils/messages'
+
 import { Message, MessageProps } from '../Message'
 
 import { styles } from './styles';
 
-let messagesQueue: MessageProps[] = [];
+let messagesQueue: MessageProps[] = MESSAGES_EXAMPLE;
 
 const socket = io(String(api.defaults.baseURL));
 socket.on('new_message', (newMessage) => {
@@ -30,7 +32,7 @@ export function MessageList(){
   useEffect(() => {
     const timer = setInterval(() => {
       if (messagesQueue.length > 0) {
-        setCurrentMessages(prevState => [messagesQueue[0], prevState[0], prevState[2]]);
+        setCurrentMessages(prevState => [messagesQueue[0], prevState[0], prevState[1]]);
         messagesQueue.shift();
       }
     }, 3000);
