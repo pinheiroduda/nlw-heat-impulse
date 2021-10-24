b365abf8e46427652311d2e70b5db6003714ef19
Eduarda <eduardapinheiro1302@gmail.com>
2021-10-24 01:12:44 -0300

    feat: creates button style

--- frontend-react-native/src/components/Button/index.tsx
+++ frontend-react-native/src/components/Button/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { TouchableOpacity, TouchableOpacityProps, Text, ColorValue } from 'react-native';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  ColorValue,
+  ActivityIndicator
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons'
 
 import { styles } from './styles';
 
@@ -7,19 +14,34 @@ type Props = {
   title: string;
   color: ColorValue;
   backgroundColor: ColorValue;
+  icon?: React.ComponentProps<typeof AntDesign>['name'];
+  isLoading?: boolean;
 }
 
-export function Button({ title, color, backgroundColor, ...rest }: Props) {
+export function Button({
+    title, 
+    color, 
+    backgroundColor, 
+    icon,
+    isLoading = false, 
+    ...rest 
+  }: Props) {
   return (
     <TouchableOpacity
       style={[styles.button, { backgroundColor }]}
+      activeOpacity={0.7}
+      disabled={isLoading}
       {...rest}
     >
-
-
+     {
+       isLoading ? <ActivityIndicator color={color}/> :      
+     <> 
+      <AntDesign name={icon} size={24} style={styles.icon} />
       <Text style={[styles.title, { color }]}>
         {title}
       </Text>
+     </>
+     }
     </TouchableOpacity>
   );
 }
